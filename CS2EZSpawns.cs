361cec21fc35e5e6e95039248708c867aa5f73d7
Kyle Surowiec <kylesurowiec@me.com>
2024-08-31 15:25:13 -0600

    ...

---
+++ CS2EZSpawns.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace CS2EZSpawns;
+
+public class CS2EZSpawns : BasePlugin
+{
+    public override string ModuleName => "CS2 EZ Spawns";
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("CS2 EZ Spawns");
+    }
+}
